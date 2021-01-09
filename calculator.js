dc401f29deb11e612e9716f7d5f1f56efbcfbe57
bu-danrit <daniel.ritchie@bryanuniversity.edu>
2021-01-09 00:24:36 -0500

    calculator

---
+++ calculator.js
@@ -0,0 +1,32 @@
+var readlineSync = require('readline-sync');
+var num1 = readlineSync.questionInt(" please give first number:");
+var num2 = readlineSync.questionInt(' please give second number:');
+var enteredOperator= readlineSync.question('how would you like to solve: add/sub/mul/div? ');
+function add(num1, num2){
+    return (num1 + num2);
+}
+function sub (num1, num2){
+    return (num1 - num2);
+}
+function mul(num1, num2){
+    return (num1 * num2);
+}
+function div(num1, num2){
+    return (num1 / num2);
+}
+if (enteredOperator == 'add'){
+    //do addtion
+    console.log("the result is ",num1 + num2);
+}
+else if(enteredOperator == 'sub'){
+    //do subtraction
+    console.log("the result is ",num1 - num2);
+}
+else if (enteredOperator == 'mul'){
+// do multiplication{
+    console.log("the result is ",num1 * num2);
+}
+else if (enteredOperator == 'div'){
+    // do division
+    console.log("the result is ",num1 / num2);
+}
